10ef3a2bda9c045d05972618f86f169680b49f82
Reyes-ITQ <reyes@stetter-itq.com>
2025-05-11 21:01:13 +0100

    PR1-car-update moving carData oout of controller and some cleanup!

--- src/apps/backend/src/controllers/car.controller.ts
+++ src/apps/backend/src/controllers/car.controller.ts
@@ -1,29 +1,18 @@
 import axios from 'axios';
-
-interface CarData {
-    brake: number;
-    date: string;
-    driver_number: number;
-    drs: number;
-    meeting_key: number;
-    n_gear: number;
-    rpm: number;
-    session_key: number;
-    speed: number;
-    throttle: number;
-}
+import { CarData } from './carData';
 
 export const getCarData = async (req: any, res: any) => {
     const { driver_number, speed } = req.query;
 
     if (!driver_number || !speed) {
-        return res.status(400).json({ error: 'Missing query parameters' });
+        return res.status(404).json({ error: 'error' });
     }
+    
 
     try {
-        const url = `https://api.openf1.org/v1/car_data?driver_number=${driver_number}&session_key=9159&speed>=${speed}`
-        const response = await axios.get<CarData[]>(url);
-        return res.json(response.data);
+        const u = `https://api.openf1.org/v1/car_data?driver_number=${driver_number}&session_key=9159&speed>=${speed}`
+        const r = await axios.get<CarData[]>(u);
+        return res.json(r.data);
     } catch (err: any) {
         console.error(err.message);
         return res.status(500).json({ error: 'Error fetching OpenF1 car data' });
